d265836b3500c9a5f383c36e0fa2ab6c7b1329f7
Sterling Orsten <sgorsten@gmail.com>
2013-10-25 16:11:56 -0700

    Use explicit spacing instead of tabs, to make it easier to read on github, and other text editors.

--- src/basic_app/basic_app.cpp
+++ src/basic_app/basic_app.cpp
@@ -6,72 +6,72 @@ using namespace cu;
 
 int main()
 {
-	// We have convenient linear algebra types available
-	float3x4 a = mul(float3x2(), float2x4()) + float3x4();
-	float4x2 b = mul(float4x3(), float3x2()) - float4x2();
-	float2x3 c = mul(float2x4(), float4x3()) * 2.0f;
-	float4x4 d = mul(float4x4(), float4x4()) / 3.0f;
-	float2x4 e = transpose(float4x2());
-	auto x = 0.6f, y = 0.8f;
-	auto f = inv(float2x2{{x,-y}, {y,x}});
-	auto g = inv(float3x3{{x,0,-y}, {0,1,0}, {y,0,x}});
-	auto h = inv(float4x4{{1,0,0,0}, {0,x,0,-y}, {0,0,1,0}, {0,y,0,x}});
-	float4x4 i = {{g.x,0}, {g.y,0}, {g.z,0}, {0,0,0,1}};
+    // We have convenient linear algebra types available
+    float3x4 a = mul(float3x2(), float2x4()) + float3x4();
+    float4x2 b = mul(float4x3(), float3x2()) - float4x2();
+    float2x3 c = mul(float2x4(), float4x3()) * 2.0f;
+    float4x4 d = mul(float4x4(), float4x4()) / 3.0f;
+    float2x4 e = transpose(float4x2());
+    auto x = 0.6f, y = 0.8f;
+    auto f = inv(float2x2{{x,-y}, {y,x}});
+    auto g = inv(float3x3{{x,0,-y}, {0,1,0}, {y,0,x}});
+    auto h = inv(float4x4{{1,0,0,0}, {0,x,0,-y}, {0,0,1,0}, {0,y,0,x}});
+    float4x4 i = {{g.x,0}, {g.y,0}, {g.z,0}, {0,0,0,1}};
 
-	// We can parse JSON from a string
-	auto parsed_value = json::parse(R"(
-	{
-		"firstName": "John",
-		"lastName": "Smith",
-		"age": 25,
-		"isEmployed": true,
-		"spouse": null,
-		"address": {
-			"streetAddress": "21 2nd Street",
-			"city": "New York",
-			"state": "NY",
-			"postalCode": 10021
-		},
-		"phoneNumbers": [
-			{
-				"type": "home",
-				"number": "212 555-1234"
-			},
-			{
-				"type": "fax",
-				"number": "646 555-4567"
-			}
-		]
-	})");
+    // We can parse JSON from a string
+    auto parsed_value = json::parse(R"(
+    {
+        "firstName": "John",
+        "lastName": "Smith",
+        "age": 25,
+        "isEmployed": true,
+        "spouse": null,
+        "address": {
+            "streetAddress": "21 2nd Street",
+            "city": "New York",
+            "state": "NY",
+            "postalCode": 10021
+        },
+        "phoneNumbers": [
+            {
+                "type": "home",
+                "number": "212 555-1234"
+            },
+            {
+                "type": "fax",
+                "number": "646 555-4567"
+            }
+        ]
+    })");
 
-	// We can also construct it using initializer lists				
-	json::value literal_value = json::object{
-		{"firstName", "John"},
-		{"lastName", "Smith"},
-		{"age", 25},
-		{"isEmployed", true},
-		{"spouse", nullptr},
-		{"address", json::object{
-			{"streetAddress", "21 2nd Street"},
-			{"city", "New York"},
-			{"state", "NY"},
-			{"postalCode", 10021}
-		}},
-		{"phoneNumbers", json::array{
-			json::object{
-				{"type", "home"},
-				{"number", "212 555-1234"}
-			},
-			json::object{
-				{"type", "fax"},
-				{"number", "646 555-4567"}
-			}
-		}}
-	};
+    // We can also construct it using initializer lists                
+    json::value literal_value = json::object{
+        {"firstName", "John"},
+        {"lastName", "Smith"},
+        {"age", 25},
+        {"isEmployed", true},
+        {"spouse", nullptr},
+        {"address", json::object{
+            {"streetAddress", "21 2nd Street"},
+            {"city", "New York"},
+            {"state", "NY"},
+            {"postalCode", 10021}
+        }},
+        {"phoneNumbers", json::array{
+            json::object{
+                {"type", "home"},
+                {"number", "212 555-1234"}
+            },
+            json::object{
+                {"type", "fax"},
+                {"number", "646 555-4567"}
+            }
+        }}
+    };
 
-	// We can easily print our json::values back into JSON
-	std::cout << "Parsed value = " << parsed_value << std::endl;
-	std::cout << "Literal value = " << literal_value << std::endl;
-	std::cout << "Are literals equal? " << (parsed_value == literal_value ? "yes" : "no") << std::endl;
-	return 0;
+    // We can easily print our json::values back into JSON
+    std::cout << "Parsed value = " << parsed_value << std::endl;
+    std::cout << "Literal value = " << literal_value << std::endl;
+    std::cout << "Are literals equal? " << (parsed_value == literal_value ? "yes" : "no") << std::endl;
+    return 0;
 }
