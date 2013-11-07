690eedf68b67c98b6c3d8f224921661c049ded7b
Sterling Orsten <sgorsten@gmail.com>
2013-11-06 16:27:39 -0800

    More small bugfixes.

--- include/cu/mesh.h
+++ include/cu/mesh.h
@@ -56,7 +56,7 @@ namespace cu
     template<class V, class T> TriMesh<V,uint8_t> boxMesh(const vec<T,3> & dims, vec<T,3> V::*position, vec<T,3> V::*normal = 0, vec<T,2> V::*texCoord = 0)
     {
         static const vec<T,3> corners[] = {{0,0,0},{0,0,1},{0,1,1},{0,1,0},{1,1,0},{1,1,1},{1,0,1},{1,0,0},{0,0,0},{1,0,0},{1,0,1},{0,0,1},{0,1,1},{1,1,1},{1,1,0},{0,1,0},{0,0,0},{0,1,0},{1,1,0},{1,0,0},{1,0,1},{1,1,1},{0,1,1},{0,0,1}};
-        static const vec<T,2> coords[] = {{0,0},{1,0},{1,1},{1,0}};
+        static const vec<T,2> coords[] = {{0,0},{1,0},{1,1},{0,1}};
         TriMesh<V,uint8_t> m = {{24,V()},{{0,1,2},{0,2,3},{4,5,6},{4,6,7},{8,9,10},{8,10,11},{12,13,14},{12,14,15},{16,17,18},{16,18,19},{20,21,22},{20,22,23}}};
         for (int i = 0; i < 24; ++i) m.verts[i].*position = corners[i] * dims - dims / 2.0f;
         if (texCoord) for (int i = 0; i<24; ++i) m.verts[i].*texCoord = coords[i % 4];
